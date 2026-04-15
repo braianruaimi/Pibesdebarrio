dff7f010eeb622b9b80bb3bfc763b6131531e64b
braianruaimi <braianruaimi@gmail.com>
2026-04-15 05:15:26 -0300

    Deploy to GitHub Pages

--- _next/static/T-zn7tXEwrPukZxdNhuR5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cff3d4d758fd1d2.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
